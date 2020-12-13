924d866e6577038a60594e7733bb8af5f7559479
Brahim Akherraz <akherraz.brahim75@gmail.com>
2020-12-13 21:40:36 +0100

    first commit

---
+++ search.py
@@ -0,0 +1,11 @@
+
+groscaca=[{
+  "url": "https://www.fnac.com/Console-Sony-PS5-Edition-Standard/a14119956/w-4#omnsearchpos=1",
+  "elts":
+    [{"XPATH":"/html/body/div[2]/div/div[1]/div[2]/section/ul[2]/li/div/div[1]/div[1]/div[1]/div/p"}]
+},
+{
+  "url": "https://www.amazon.fr/PlayStation-%C3%89dition-Standard-DualSense-Couleur/dp/B08H93ZRK9/ref=sr_1_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=playstation+5&qid=1607891104&sr=8-1",
+  "elts":
+    [{"XPATH":"/html/body/div[2]/div[2]/div[4]/div[5]/div[1]/div[2]/div/div/div/div/div/form/div/div/div[1]/span"}]
+}]
